f9a6c553dbd1f5f757b0b2c48d138ba00b6de64a
farshad-vgr <farshadvaghari@gmail.com>
2022-11-03 16:28:58 +0330

    Drag and Drop to modify item's order added

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const ul = document.getElementById("my-ul");
+
+ul.addEventListener("dragover", (event) => {
+  event.preventDefault();
+});
+
+ul.addEventListener("dragenter", (e) => {
+  if (e.target.classList.contains("card") && !e.target.classList.contains("dragging")) {
+    const draggingCard = document.querySelector(".dragging");
+    const cards = [...ul.querySelectorAll("li")];
+    const currentPosition = cards.indexOf(draggingCard);
+    const newPosition = cards.indexOf(e.target);
+
+    if (currentPosition > newPosition) {
+      ul.insertBefore(draggingCard, e.target);
+    } else {
+      ul.insertBefore(draggingCard, e.target.nextSibling);
+    }
+  }
+});
+
 function loadUsers() {
   const xhr = new XMLHttpRequest();
 
@@ -10,18 +31,26 @@ function loadUsers() {
 
       for (const user of users) {
         const li = document.createElement("li");
+        li.classList.add("card");
+        li.setAttribute("draggable", "true");
         li.innerHTML = `
-            <img src="${user.avatar_url}">
-                <section>
-                    <h3>User name: ${user.login}</h3>
-                    <a href="${
-                      user.html_url
-                    }" target="_blank">Visit profile on Github!</a>
-                    <span>Followers: ${Math.ceil(
-                      Math.random() * user.login.length
-                    )}K</span>
-                </section>
+          <div>
+            <img src="${user.avatar_url}" draggable="false">
+              <section>
+                <h3>User name: ${user.login}</h3>
+                <a href="${user.html_url}" target="_blank">Visit profile on Github!</a>
+                <span>Followers: ${Math.ceil(Math.random() * user.login.length)}K</span>
+              </section>
+            </div>
           `;
+
+        li.addEventListener("dragstart", () => {
+          li.classList.add("dragging");
+        });
+
+        li.addEventListener("dragend", () => {
+          li.classList.remove("dragging");
+        });
 
         ul.append(li);
       }
